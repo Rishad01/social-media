b619fe5ef901b2bad7eb060e428ba5894a712cfe
Rishad01 <103144537+Rishad01@users.noreply.github.com>
2024-06-22 05:21:26 +0530

    Update Login.jsx

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
     event.preventDefault();
     console.log('login');
     try {
-      const response = await axios.post(`social-media-three-puce.vercel.app/login`, values);
+      const response = await axios.post(`https://social-media-back-ho56.onrender.com/login`, values);
       console.log(response);
       if (response.data.token) {
         localStorage.setItem('token', response.data.token);
