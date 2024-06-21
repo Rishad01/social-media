1f1fbc483ef8db9d61dd23b34b070f454513e9c7
Rishad01 <rishadhusainrizvi786@gmail.com>
2024-06-21 17:32:05 +0530

    abc

--- backend/index.js
+++ backend/index.js
@@ -127,14 +127,14 @@ try{
         }
       });
 
-      console.log(req.headers.host);
+      
       const mailOptions = {
         from: 'rhr111984@gmail.com',
         to: user.email,
         subject: 'Password Reset Request',
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n`
           + `Please click on the following link, or paste this into your browser to complete the process:\n\n`
-          + `http://localhost:3000/reset/${resetToken}\n\n`
+          + `http://${req.headers.host}/reset/${resetToken}\n\n`
           + `If you did not request this, please ignore this email and your password will remain unchanged.\n`
       };
   
